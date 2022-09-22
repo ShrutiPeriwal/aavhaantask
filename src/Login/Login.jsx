b763d833b75369551f67d0837d4234690f241504
Shruti <shrutiperiwal21@gmail.com>
2022-09-22 12:01:30 +0530

    login

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,40 +1,154 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+// import React, {useState} from 'react';
+// import axios from 'axios';
 
-const Login = () => {
+// const Login = () => {
 
-  const [username, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+//   const [username, setUserName] = useState("");
+//   const [password, setPassword] = useState("");
 
-  const [loginStatus, setLoginStatus] = useState("");
+//   const [loginStatus, setLoginStatus] = useState("");
 
-  const login = async (req, res) => {
-    const response = await axios.post("http://localhost:3001/login", {
-      username: username,
-      password: password,
-    })
-    if(response.data.message) {
-    setLoginStatus(response.data.message)
-    } else {
-      setLoginStatus(response.data[0].username)
-    }
-  }
+//   const login = async (req, res) => {
+//     const response = await axios.post("http://localhost:3001/login", {
+//       username: username,
+//       password: password,
+//     })
+//     if(response.data.message) {
+//     setLoginStatus(response.data.message)
+//     } else {
+//       setLoginStatus(response.data[0].username)
+//     }
+//   }
   
+//   return (
+//     <div className="App">
+//         <div className="login">
+//           <h1>Login Page</h1>
+//           <input type="text" placeholder="username" onChange={(e) => { 
+//             setUserName(e.target.value);
+//           }}/>
+//           <input type="password" placeholder="password" onChange={(e) => {
+//             setPassword(e.target.value);
+//           }}/>
+//           <button onClick={() => {login()}}>Login</button>
+//         </div>
+//         <h1>{loginStatus}</h1>
+//     </div>
+//   )
+// }
+
+// export default Login
+
+
+
+
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function Copyright(props) {
   return (
-    <div className="App">
-        <div className="login">
-          <h1>Login Page</h1>
-          <input type="text" placeholder="username" onChange={(e) => { 
-            setUserName(e.target.value);
-          }}/>
-          <input type="password" placeholder="password" onChange={(e) => {
-            setPassword(e.target.value);
-          }}/>
-          <button onClick={() => {login()}}>Login</button>
-        </div>
-        <h1>{loginStatus}</h1>
-    </div>
-  )
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default Login
+const theme = createTheme();
+
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
